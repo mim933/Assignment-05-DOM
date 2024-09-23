d10617082ae24b2df355dda92ac3c58a472a3472
mim933 <mdyousufmim933@gmail.com>
2024-09-23 23:03:12 +0600

    added js file

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const donateButton = document.getElementById('donate-1-btn');
+donateButton.addEventListener('click', function(){
+    const inputField =   document.getElementById('input-1-field');
+    console.log(inputField.value);
+    document.getElementById('balance').innerHTML= inputField.value;
+    const mainBalance = document.getElementById('main-balance');
+    const subtractBalance=parseFloat(mainBalance.innerHTML) - parseFloat(inputField.value);
+    mainBalance.innerHTML = subtractBalance;
+    console.log(subtractBalance)
+})
