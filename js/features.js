01faf13b59771ffad1917911b28a09609adc5cb1
mim933 <mdyousufmim933@gmail.com>
2024-09-25 02:52:20 +0600

    added history section

--- js/features.js
+++ js/features.js
@@ -8,4 +8,24 @@ document.getElementById('show-history').addEventListener('click', function(){
     showSectionById('Transaction-section')
 })
 
+// document.getElementById('donate-button')
+// .addEventListener('click', function(){
+//     console.log('show donate button');
+//     showSectionById('donation-field2');
+// })
+
+// document.getElementById('show-history').addEventListener('click', function(){
+//     showSectionById('Transaction-section')
+// })
+
+// document.getElementById('donate-button')
+// .addEventListener('click', function(){
+//     console.log('show donate button');
+//     showSectionById('donation-field3');
+// })
+
+// document.getElementById('show-history').addEventListener('click', function(){
+//     showSectionById('Transaction-section')
+// })
+
 
--- js/utilities.js
+++ js/utilities.js
@@ -1,8 +1,14 @@
 function showSectionById(id){
     document.getElementById('donation-field').classList.add('hidden');
-    document.getElementById('Transaction-section').classList.add('hidden');                                                                                                                                                                      
+    document.getElementById('Transaction-section').classList.add('hidden');       
+    // document.getElementById('Transaction-section').classList.add('hidden')                                                                                                                                                            
+    // document.getElementById('Transaction-section').classList.add('hidden')                                                                                                                                                            
     
     document.getElementById(id).classList.remove('hidden');
+    // document.getElementById(id).classList.remove('hidden');
+    // document.getElementById(id).classList.remove('hidden');
+    // document.getElementById(id).classList.remove('hidden');
+
     
    
 
